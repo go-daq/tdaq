9bd5a7628e51c889617e55575b5da62563eb2929
Sebastien Binet <binet@cern.ch>
2019-09-10 12:19:38 +0200

    tdaq/config: add doc strings

--- config/config.go
+++ config/config.go
@@ -9,21 +9,23 @@ import (
 	"github.com/go-daq/tdaq/log"
 )
 
+// Process describes how a TDAQ process should be configured.
 type Process struct {
-	Name   string
-	Level  log.Level
-	RunCtl string
+	Name   string    // name of the TDAQ process
+	Level  log.Level // verbosity level of the TDAQ process
+	RunCtl string    // address of the run-ctl of the flock of TDAQ processes
 
-	Args []string
+	Args []string // additional flag arguments
 }
 
+// RunCtl describes how a TDAQ RunControl process should be configured.
 type RunCtl struct {
-	Name   string
-	Level  log.Level
-	RunCtl string
-	Web    string
+	Name   string    // name of the run-ctl process.
+	Level  log.Level // verbosity level of the run-ctl process.
+	RunCtl string    // address of the TCP run-ctl server
+	Web    string    // address of the HTTP run-ctl server
 
-	Interactive bool
+	Interactive bool // enable interactive shell commands for the run-ctl process
 
-	Args []string
+	Args []string // additional flag arguments
 }
