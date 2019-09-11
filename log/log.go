64f29fe64cf24aad98dcecf5a01402f25ef70877
Sebastien Binet <binet@cern.ch>
2019-09-11 10:42:11 +0200

    log: remove WriteSyncer interface

--- log/log.go
+++ log/log.go
@@ -62,19 +62,11 @@ type MsgStream interface {
 	Errorf(format string, a ...interface{}) (int, error)
 
 	Msg(lvl Level, format string, a ...interface{}) (int, error)
-
-	Sync() error
-}
-
-// WriteSyncer is an io.Writer which can be sync'ed/flushed.
-type WriteSyncer interface {
-	io.Writer
-	Sync() error
 }
 
 type msgstream struct {
 	lvl Level
-	w   WriteSyncer
+	w   io.Writer
 	n   string
 }
 
@@ -118,11 +110,11 @@ func Panicf(format string, a ...interface{}) (int, error) {
 // NewMsgStream creates a new MsgStream value with name name and minimum
 // verbosity level lvl.
 // This MsgStream will print messages into w.
-func NewMsgStream(name string, lvl Level, w WriteSyncer) MsgStream {
+func NewMsgStream(name string, lvl Level, w io.Writer) MsgStream {
 	return newMsgStream(name, lvl, w)
 }
 
-func newMsgStream(name string, lvl Level, w WriteSyncer) msgstream {
+func newMsgStream(name string, lvl Level, w io.Writer) msgstream {
 	if w == nil {
 		w = os.Stdout
 	}
@@ -146,13 +138,11 @@ func (msg msgstream) Infof(format string, a ...interface{}) (int, error) {
 
 // Warnf displays a (formated) WARN message
 func (msg msgstream) Warnf(format string, a ...interface{}) (int, error) {
-	defer msg.flush()
 	return msg.Msg(LvlWarning, format, a...)
 }
 
 // Errorf displays a (formated) ERR message
 func (msg msgstream) Errorf(format string, a ...interface{}) (int, error) {
-	defer msg.flush()
 	return msg.Msg(LvlError, format, a...)
 }
 
@@ -167,12 +157,6 @@ func (msg msgstream) Msg(lvl Level, format string, a ...interface{}) (int, error
 	}
 	format = msg.n + lvl.MsgString() + " " + format + eol
 	return fmt.Fprintf(msg.w, format, a...)
-}
-
-func (msg msgstream) Sync() error { return msg.flush() }
-
-func (msg msgstream) flush() error {
-	return msg.w.Sync()
 }
 
 var (
