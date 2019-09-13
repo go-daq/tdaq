78a5f003fdb488fc7249a6ba71ab5a96db90bbdd
Sebastien Binet <binet@cern.ch>
2019-09-13 12:08:15 +0200

    cmd/tdaq-i64-gen: first import

---
+++ cmd/tdaq-i64-gen/main.go
@@ -0,0 +1,101 @@
+// Copyright 2019 The go-daq Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Command tdaq-i64-gen is a simple program that generates int64 data.
+package main
+
+import (
+	"context"
+	"encoding/binary"
+	"io"
+	"os"
+	"time"
+
+	"github.com/go-daq/tdaq"
+	"github.com/go-daq/tdaq/flags"
+	"github.com/go-daq/tdaq/log"
+)
+
+func main() {
+	cmd := flags.New()
+
+	dev := counter{
+		start: 10,
+	}
+
+	srv := tdaq.New(cmd, os.Stdout)
+	srv.CmdHandle("/init", dev.OnInit)
+	srv.CmdHandle("/start", dev.OnStart)
+	srv.CmdHandle("/stop", dev.OnStop)
+	srv.CmdHandle("/quit", dev.OnQuit)
+
+	srv.OutputHandle("/adc", dev.adc)
+
+	srv.RunHandle(dev.run)
+
+	err := srv.Run(context.Background())
+	if err != nil {
+		log.Panicf("error: %v", err)
+	}
+}
+
+type counter struct {
+	start int64
+
+	n  int64
+	ch chan int64
+}
+
+func (dev *counter) OnInit(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /init command...")
+	dev.n = dev.start
+	dev.ch = make(chan int64)
+	return nil
+}
+
+func (dev *counter) OnStart(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /start command...")
+	return nil
+}
+
+func (dev *counter) OnStop(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	n := dev.n
+	ctx.Msg.Debugf("received /stop command... -> n=%d", n)
+	return nil
+}
+
+func (dev *counter) OnQuit(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received %q command...", req.Path)
+	return nil
+}
+
+func (dev *counter) adc(ctx tdaq.Context, dst *tdaq.Frame) error {
+	select {
+	case <-ctx.Ctx.Done():
+		dst.Body = nil
+		return io.EOF
+	case data := <-dev.ch:
+		dst.Body = make([]byte, 8)
+		binary.LittleEndian.PutUint64(dst.Body, uint64(data))
+	}
+	return nil
+}
+
+func (dev *counter) run(ctx tdaq.Context) error {
+	for {
+		select {
+		case <-ctx.Ctx.Done():
+			return nil
+		default:
+			select {
+			case dev.ch <- dev.n:
+				dev.n++
+			default:
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	return nil
+}
