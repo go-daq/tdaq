a038c0ff2c7122a12f32b4a0634aaa106c4da392
Sebastien Binet <binet@cern.ch>
2019-09-13 12:08:47 +0200

    cmd/tdaq-i64-print: first import

---
+++ cmd/tdaq-i64-print/main.go
@@ -0,0 +1,78 @@
+// Copyright 2019 The go-daq Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Command tdaq-i64-print is a simple program that consumes int64 data.
+package main
+
+import (
+	"context"
+	"encoding/binary"
+	"os"
+
+	"github.com/go-daq/tdaq"
+	"github.com/go-daq/tdaq/flags"
+	"github.com/go-daq/tdaq/log"
+)
+
+func main() {
+	cmd := flags.New()
+
+	dev := device{}
+	srv := tdaq.New(cmd, os.Stdout)
+	srv.CmdHandle("/init", dev.OnInit)
+	srv.CmdHandle("/start", dev.OnStart)
+	srv.CmdHandle("/stop", dev.OnStop)
+	srv.CmdHandle("/quit", dev.OnQuit)
+
+	srv.InputHandle("/adc", dev.adc)
+
+	err := srv.Run(context.Background())
+	if err != nil {
+		log.Panicf("error: %v", err)
+	}
+}
+
+type device struct {
+	n int64
+}
+
+func (dev *device) OnConfig(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /config command...")
+	return nil
+}
+
+func (dev *device) OnInit(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /init command...")
+	dev.n = 0
+	return nil
+}
+
+func (dev *device) OnReset(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /reset command...")
+	dev.n = 0
+	return nil
+}
+
+func (dev *device) OnStart(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /start command...")
+	return nil
+}
+
+func (dev *device) OnStop(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	n := dev.n
+	ctx.Msg.Debugf("received /stop command... -> n=%d", n)
+	return nil
+}
+
+func (dev *device) OnQuit(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received %q command...", req.Path)
+	return nil
+}
+
+func (dev *device) adc(ctx tdaq.Context, src tdaq.Frame) error {
+	dev.n = int64(binary.LittleEndian.Uint64(src.Body))
+	ctx.Msg.Debugf("received: %d -> %v", len(src.Body), dev.n)
+	dev.n++
+	return nil
+}
