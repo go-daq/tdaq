07d4174765d92195b99265a020ff7e4e74bb7370
Sebastien Binet <binet@cern.ch>
2019-09-18 16:04:40 +0200

    xdaq: add splitter process
    Fixes go-daq/tdaq#19.

---
+++ xdaq/splitter.go
@@ -0,0 +1,103 @@
+// Copyright 2019 The go-daq Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package xdaq // import "github.com/go-daq/tdaq/xdaq"
+
+import (
+	"github.com/go-daq/tdaq"
+)
+
+// Splitter consumes data from an input end-point and publishes it
+// on either the "left" or "right" output end-point, whether the Fct function
+// returns (resp.) -1 or +1.
+type Splitter struct {
+	Fct func() int // decides whether to forward the input data on the "left" or the "right".
+
+	left  chan tdaq.Frame
+	right chan tdaq.Frame
+
+	acc int64 // number of left-accepted input data
+	tot int64 // total number of input data
+}
+
+func (dev *Splitter) OnConfig(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /config command...")
+	return nil
+}
+
+func (dev *Splitter) OnInit(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /init command...")
+	if dev.Fct == nil {
+		dev.Fct = func() int { return -1 }
+	}
+	dev.left = make(chan tdaq.Frame)
+	dev.right = make(chan tdaq.Frame)
+	dev.acc = 0
+	dev.tot = 0
+	return nil
+}
+
+func (dev *Splitter) OnReset(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /reset command...")
+	dev.left = make(chan tdaq.Frame)
+	dev.right = make(chan tdaq.Frame)
+	dev.acc = 0
+	dev.tot = 0
+	return nil
+}
+
+func (dev *Splitter) OnStart(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /start command...")
+	return nil
+}
+
+func (dev *Splitter) OnStop(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	n := dev.tot
+	v := dev.acc
+	ctx.Msg.Infof("received /stop command... v=%d/%d -> %g%%", n, v, float64(v)/float64(n)*100)
+	return nil
+}
+
+func (dev *Splitter) OnQuit(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+	ctx.Msg.Debugf("received /quit command...")
+	return nil
+}
+
+func (dev *Splitter) Input(ctx tdaq.Context, src tdaq.Frame) error {
+	dev.tot++
+	switch dev.Fct() {
+	case -1:
+		dev.acc++
+		ctx.Msg.Debugf("received: %d/%d (left)", dev.acc, dev.tot)
+		dev.left <- src
+	default:
+		ctx.Msg.Debugf("received: %d/%d (right)", dev.tot-dev.acc, dev.tot)
+		dev.right <- src
+	}
+	return nil
+}
+
+func (dev *Splitter) Left(ctx tdaq.Context, dst *tdaq.Frame) error {
+	select {
+	case <-ctx.Ctx.Done():
+		dst.Body = nil
+		return nil
+	case data := <-dev.left:
+		dst.Body = make([]byte, len(data.Body))
+		copy(dst.Body, data.Body)
+	}
+	return nil
+}
+
+func (dev *Splitter) Right(ctx tdaq.Context, dst *tdaq.Frame) error {
+	select {
+	case <-ctx.Ctx.Done():
+		dst.Body = nil
+		return nil
+	case data := <-dev.right:
+		dst.Body = make([]byte, len(data.Body))
+		copy(dst.Body, data.Body)
+	}
+	return nil
+}
--- xdaq/xdaq_test.go
+++ xdaq/xdaq_test.go
@@ -545,3 +545,347 @@ loop:
 		t.Fatalf("stage-3: error: %q:%v, %q:%v", proc1.name, *proc1.v, proc5.name, *proc5.v)
 	}
 }
+
+func TestSplitter(t *testing.T) {
+	t.Parallel()
+
+	const (
+		rclvl   = log.LvlDebug
+		proclvl = log.LvlDebug
+		nprocs  = 7
+	)
+
+	port, err := tcputil.GetTCPPort()
+	if err != nil {
+		t.Fatalf("could not find a tcp port for run-ctl: %+v", err)
+	}
+
+	rcAddr := ":" + port
+
+	port, err = tcputil.GetTCPPort()
+	if err != nil {
+		t.Fatalf("could not find a tcp port for run-ctl web server: %+v", err)
+	}
+	webAddr := ":" + port
+
+	wbuf := new(bytes.Buffer)
+	stdout := iomux.NewWriter(wbuf)
+
+	fname, err := ioutil.TempFile("", "tdaq-")
+	if err != nil {
+		t.Fatalf("could not create a temporary log file for run-ctl log server: %+v", err)
+	}
+	fname.Close()
+	defer func() {
+		if err != nil {
+			raw, err := ioutil.ReadFile(fname.Name())
+			if err == nil {
+				t.Logf("log-file:\n%v\n", string(raw))
+			}
+		}
+		os.Remove(fname.Name())
+	}()
+
+	cfg := config.RunCtl{
+		Name:      "run-ctl",
+		Level:     rclvl,
+		RunCtl:    rcAddr,
+		Web:       webAddr,
+		LogFile:   fname.Name(),
+		HBeatFreq: 50 * time.Millisecond,
+	}
+
+	rc, err := tdaq.NewRunControl(cfg, stdout)
+	if err != nil {
+		t.Fatalf("could not create run-ctl: %+v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+
+	grp, ctx := errgroup.WithContext(ctx)
+
+	errc := make(chan error)
+	go func() {
+		errc <- rc.Run(ctx)
+	}()
+
+	type state struct {
+		name string
+		v    *int64
+	}
+
+	var (
+		proc1 = state{name: "proc-1"}
+		proc2 = state{name: "proc-2"}
+		proc3 = state{name: "proc-3"}
+		proc4 = state{name: "proc-4"}
+		proc5 = state{name: "proc-5"}
+		proc6 = state{name: "proc-6"}
+		proc7 = state{name: "proc-7"}
+	)
+
+	grp.Go(func() error {
+		dev := xdaq.I64Gen{}
+		proc1.v = &dev.N
+
+		cfg := config.Process{
+			Name:   proc1.name,
+			Level:  proclvl,
+			RunCtl: rcAddr,
+		}
+		srv := tdaq.New(cfg, stdout)
+		srv.CmdHandle("/config", dev.OnConfig)
+		srv.CmdHandle("/init", dev.OnInit)
+		srv.CmdHandle("/start", dev.OnStart)
+		srv.CmdHandle("/stop", dev.OnStop)
+		srv.CmdHandle("/reset", dev.OnReset)
+		srv.CmdHandle("/quit", dev.OnQuit)
+
+		srv.OutputHandle("/i64", dev.Output)
+		srv.RunHandle(dev.Loop)
+
+		err := srv.Run(ctx)
+		return err
+	})
+
+	grp.Go(func() error {
+		var n int64
+		dev := xdaq.Splitter{
+			Fct: func() int {
+				n++
+				switch {
+				case n%2 == 0:
+					return -1
+				default:
+					return +1
+				}
+			},
+		}
+		proc2.v = &n
+
+		cfg := config.Process{
+			Name:   proc2.name,
+			Level:  proclvl,
+			RunCtl: rcAddr,
+		}
+		srv := tdaq.New(cfg, stdout)
+		srv.CmdHandle("/config", dev.OnConfig)
+		srv.CmdHandle("/init", dev.OnInit)
+		srv.CmdHandle("/start", dev.OnStart)
+		srv.CmdHandle("/stop", dev.OnStop)
+		srv.CmdHandle("/reset", func(ctx tdaq.Context, resp *tdaq.Frame, req tdaq.Frame) error {
+			n = 0
+			dev.Fct = func() int {
+				n++
+				switch {
+				case n%2 == 0:
+					return -1
+				default:
+					return +1
+				}
+			}
+			return dev.OnReset(ctx, resp, req)
+		})
+		srv.CmdHandle("/quit", dev.OnQuit)
+
+		srv.InputHandle("/i64", dev.Input)
+		srv.OutputHandle("/i64-1-left", dev.Left)
+		srv.OutputHandle("/i64-1-right", dev.Right)
+
+		err := srv.Run(ctx)
+		return err
+	})
+
+	grp.Go(func() error {
+		dev := xdaq.I64Dumper{}
+		proc3.v = &dev.N
+
+		cfg := config.Process{
+			Name:   proc3.name,
+			Level:  proclvl,
+			RunCtl: rcAddr,
+		}
+		srv := tdaq.New(cfg, stdout)
+		srv.CmdHandle("/config", dev.OnConfig)
+		srv.CmdHandle("/init", dev.OnInit)
+		srv.CmdHandle("/start", dev.OnStart)
+		srv.CmdHandle("/stop", dev.OnStop)
+		srv.CmdHandle("/reset", dev.OnReset)
+		srv.CmdHandle("/quit", dev.OnQuit)
+
+		srv.InputHandle("/i64-1-left", dev.Input)
+
+		err := srv.Run(ctx)
+		return err
+	})
+
+	grp.Go(func() error {
+		dev := xdaq.I64Dumper{}
+		proc4.v = &dev.N
+
+		cfg := config.Process{
+			Name:   proc4.name,
+			Level:  proclvl,
+			RunCtl: rcAddr,
+		}
+		srv := tdaq.New(cfg, stdout)
+		srv.CmdHandle("/config", dev.OnConfig)
+		srv.CmdHandle("/init", dev.OnInit)
+		srv.CmdHandle("/start", dev.OnStart)
+		srv.CmdHandle("/stop", dev.OnStop)
+		srv.CmdHandle("/reset", dev.OnReset)
+		srv.CmdHandle("/quit", dev.OnQuit)
+
+		srv.InputHandle("/i64-1-right", dev.Input)
+
+		err := srv.Run(ctx)
+		return err
+	})
+
+	grp.Go(func() error {
+		dev := xdaq.Splitter{}
+		cfg := config.Process{
+			Name:   proc5.name,
+			Level:  proclvl,
+			RunCtl: rcAddr,
+		}
+		srv := tdaq.New(cfg, stdout)
+		srv.CmdHandle("/config", dev.OnConfig)
+		srv.CmdHandle("/init", dev.OnInit)
+		srv.CmdHandle("/start", dev.OnStart)
+		srv.CmdHandle("/stop", dev.OnStop)
+		srv.CmdHandle("/reset", dev.OnReset)
+		srv.CmdHandle("/quit", dev.OnQuit)
+
+		srv.InputHandle("/i64", dev.Input)
+		srv.OutputHandle("/i64-2-left", dev.Left)
+		srv.OutputHandle("/i64-2-right", dev.Right)
+
+		err := srv.Run(ctx)
+		return err
+	})
+
+	grp.Go(func() error {
+		dev := xdaq.I64Dumper{}
+		proc6.v = &dev.N
+
+		cfg := config.Process{
+			Name:   proc6.name,
+			Level:  proclvl,
+			RunCtl: rcAddr,
+		}
+		srv := tdaq.New(cfg, stdout)
+		srv.CmdHandle("/config", dev.OnConfig)
+		srv.CmdHandle("/init", dev.OnInit)
+		srv.CmdHandle("/start", dev.OnStart)
+		srv.CmdHandle("/stop", dev.OnStop)
+		srv.CmdHandle("/reset", dev.OnReset)
+		srv.CmdHandle("/quit", dev.OnQuit)
+
+		srv.InputHandle("/i64-2-left", dev.Input)
+
+		err := srv.Run(ctx)
+		return err
+	})
+
+	grp.Go(func() error {
+		dev := xdaq.I64Dumper{}
+		proc7.v = &dev.N
+
+		cfg := config.Process{
+			Name:   proc7.name,
+			Level:  proclvl,
+			RunCtl: rcAddr,
+		}
+		srv := tdaq.New(cfg, stdout)
+		srv.CmdHandle("/config", dev.OnConfig)
+		srv.CmdHandle("/init", dev.OnInit)
+		srv.CmdHandle("/start", dev.OnStart)
+		srv.CmdHandle("/stop", dev.OnStop)
+		srv.CmdHandle("/reset", dev.OnReset)
+		srv.CmdHandle("/quit", dev.OnQuit)
+
+		srv.InputHandle("/i64-2-right", dev.Input)
+
+		err := srv.Run(ctx)
+		return err
+	})
+
+	timeout := time.NewTimer(5 * time.Second)
+	defer timeout.Stop()
+loop:
+	for {
+		select {
+		case <-timeout.C:
+			t.Fatalf("devices did not connect")
+		default:
+			n := rc.NumClients()
+			if n == nprocs {
+				break loop
+			}
+		}
+	}
+
+	for _, tt := range []struct {
+		name string
+		cmd  tdaq.CmdType
+	}{
+		{"/config", tdaq.CmdConfig},
+		{"/init", tdaq.CmdInit},
+		{"/reset", tdaq.CmdReset},
+		{"/config", tdaq.CmdConfig},
+		{"/init", tdaq.CmdInit},
+		{"/start", tdaq.CmdStart},
+		{"/stop", tdaq.CmdStop},
+		{"/status", tdaq.CmdStatus},
+		{"/start", tdaq.CmdStart},
+		{"/stop", tdaq.CmdStop},
+		{"/quit", tdaq.CmdQuit},
+	} {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		func() {
+			defer cancel()
+			err = rc.Do(ctx, tt.cmd)
+			if err != nil {
+				t.Fatalf("could not send command %v: %+v", tt.cmd, err)
+			}
+			if tt.name == "/start" {
+				time.Sleep(100 * time.Millisecond)
+			}
+			if tt.name == "/stop" {
+				switch {
+				case *proc1.v != *proc2.v:
+					t.Fatalf("stage-1: error: %q:%v, %q:%v", proc1.name, *proc1.v, proc2.name, *proc2.v)
+				case *proc2.v != *proc3.v+*proc4.v:
+					t.Fatalf("stage-2: error: %q:%v, %q:%v %q:%v", proc2.name, *proc2.v, proc3.name, *proc3.v, proc4.name, *proc4.v)
+				case *proc1.v != *proc6.v+*proc7.v:
+					t.Fatalf("stage-3: error: %q:%v, %q:%v %q:%v", proc1.name, *proc1.v, proc6.name, *proc6.v, proc7.name, *proc7.v)
+				case *proc7.v != 0:
+					t.Fatalf("stage-4: error: %q:%v, %q:%v", proc1.name, *proc1.v, proc7.name, *proc7.v)
+				}
+			}
+		}()
+	}
+
+	err = grp.Wait()
+	if err != nil {
+		t.Fatalf("could not run device run-group: %+v", err)
+	}
+
+	err = <-errc
+	if err != nil && !xerrors.Is(err, context.Canceled) {
+		t.Fatalf("error shutting down run-ctl: %+v", err)
+	}
+
+	switch {
+	case *proc1.v != *proc2.v:
+		t.Fatalf("stage-1: error: %q:%v, %q:%v", proc1.name, *proc1.v, proc2.name, *proc2.v)
+	case *proc2.v != *proc3.v+*proc4.v:
+		t.Fatalf("stage-2: error: %q:%v, %q:%v %q:%v", proc2.name, *proc2.v, proc3.name, *proc3.v, proc4.name, *proc4.v)
+	case *proc1.v != *proc6.v+*proc7.v:
+		t.Fatalf("stage-3: error: %q:%v, %q:%v %q:%v", proc1.name, *proc1.v, proc6.name, *proc6.v, proc7.name, *proc7.v)
+	case *proc7.v != 0:
+		t.Fatalf("stage-4: error: %q:%v, %q:%v", proc1.name, *proc1.v, proc7.name, *proc7.v)
+	}
+}
